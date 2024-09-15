b488ac6ceac2c5dc36ea1a2d8645965126ecc654
pedrohfsouza <pedrohfsouza10@gmail.com>
2024-09-15 20:00:41 -0300

    adicionadas funcoes com a monada State para rodar o jogo

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,24 @@
 module Main (main) where
 
-import Lib
+import Data
+
+import Control.Monad
+import System.IO (hFlush, stdout)
+
+
+
 
 main :: IO ()
-main = someFunc
+main = do
+    -- Inicializar o estado do jogo
+    let estadoInicial = EstadoJogo {
+            baralho = criarBaralho,
+            cartasJogador = take 3 criarBaralho,  -- Distribuir cartas iniciais
+            cartasMaquina = take 3 (drop 3 criarBaralho),
+            manilha = determinarManilha (criarBaralho !! 6), -- Definir manilha com uma carta qualquer do baralho
+            pontosJogador = 0,
+            pontosMaquina = 0
+        }
+
+    -- Rodar o jogo usando a monad State
+    jogoTruco estadoInicial
